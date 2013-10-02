f1835149c88ef43f3b475d883a621fb8a22d1800
Thomas Steur <thomas.steur@gmail.com>
2013-10-02 02:32:29 +0000

    refs #4134 hook name changed

--- VisitorGenerator.php
+++ VisitorGenerator.php
@@ -24,7 +24,7 @@ class VisitorGenerator extends \Piwik\Plugin
     public function getListHooksRegistered()
     {
         return array(
-            'AdminMenu.addMenuEntries' => 'addMenu',
+            'Menu.Admin.addItems' => 'addMenu',
         );
     }
 
