988929f7ae8e253affbcc079d4526d21cd874a35
Fabian Becker <halfdan@xnorfz.de>
2014-02-03 00:16:52 +0100

    Removing @package/@subpackage/@category

--- Controller.php
+++ Controller.php
@@ -5,8 +5,6 @@
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
- * @category Piwik_Plugins
- * @package VisitorGenerator
  */
 namespace Piwik\Plugins\VisitorGenerator;
 
@@ -27,7 +25,6 @@ use Piwik\View;
 
 /**
  *
- * @package VisitorGenerator
  */
 class Controller extends ControllerAdmin
 {
--- VisitorGenerator.php
+++ VisitorGenerator.php
@@ -5,8 +5,6 @@
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
- * @category Piwik_Plugins
- * @package VisitorGenerator
  */
 namespace Piwik\Plugins\VisitorGenerator;
 
@@ -15,7 +13,6 @@ use Piwik\Piwik;
 
 /**
  *
- * @package VisitorGenerator
  */
 class VisitorGenerator extends \Piwik\Plugin
 {
