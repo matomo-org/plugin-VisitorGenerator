7e856090202d6bdc738a2672052ad62c213c8764
Benaka <diosmosis@users.noreply.github.com>
2018-03-25 18:14:36 -0700

    Add new command to log visits live as if they were from real incoming traffic. (#25)
    * Add visitorgenerator:generate-live-visits command that replays visits from a log as if they were coming in live.
    
    Includes some fixes to log parsing regex too.
    
    * Undo plugin.json changes, they are unnecessary.
    
    * Add another debug log for debugging new live command.

---
+++ Iterator/TransformIterator.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\VisitorGenerator\Iterator;
+
+use Iterator;
+
+class TransformIterator implements \OuterIterator
+{
+    /**
+     * @var Iterator
+     */
+    private $iterator;
+
+    /**
+     * @var callable
+     */
+    private $transform;
+
+    public function __construct($iterator, $transform)
+    {
+        $this->iterator = $iterator;
+        $this->transform = $transform;
+    }
+
+    public function current()
+    {
+        $fn = $this->transform;
+        return $fn($this->iterator->current(), $this->iterator->key(), $this->iterator);
+    }
+
+    public function next()
+    {
+        $this->iterator->next();
+    }
+
+    public function key()
+    {
+        return $this->iterator->key();
+    }
+
+    public function valid()
+    {
+        return $this->iterator->valid();
+    }
+
+    public function rewind()
+    {
+        $this->iterator->rewind();
+    }
+
+    public function getInnerIterator()
+    {
+        return $this->iterator;
+    }
+}
--- tests/Integration/LogParserTest.php
+++ tests/Integration/LogParserTest.php
@@ -60,11 +60,24 @@ class LogParserTest extends \PHPUnit_Framework_TestCase
         $expected = array(
             'ip'       => '-',
             'time'     => '29/May/2011:06:18:38 +0200',
-            'url'      => '/piwik.php?idsite=1&rec=1&apiv=1&rand=1636495582&_id=e4002ba0f2c2dd70&fla=1&java=1&dir=0&qt=0&realp=0&pdf=0&wma=0&gears=0&ag=0&h=12&m=34&s=6&res=1024x768&cookie=1&_cvar=%7B%225%22%3A%5B%22VisitorType%22%2C%22NewLoggedOut%22%5D%7D&cvar=%7B%223%22%3A%5B%22_pks%22%2C%22SKU2%22%5D%2C%224%22%3A%5B%22_pkn%22%2C%22PRODUCT+name%22%5D%2C%225%22%3A%5B%22_pkc%22%2C%22Electronics+%26+Cameras%22%5D%7D&url=http%3A%2F%2Fexample.org%2Findex.htm&urlref=&action_name=incredible+title%21&urlref=http%3A%2F%2Fpiwik.org%2Fcontribute% HTTP/1.1',
+            'url'      => '/piwik.php?idsite=1&rec=1&apiv=1&rand=1636495582&_id=e4002ba0f2c2dd70&fla=1&java=1&dir=0&qt=0&realp=0&pdf=0&wma=0&gears=0&ag=0&h=12&m=34&s=6&res=1024x768&cookie=1&_cvar=%7B%225%22%3A%5B%22VisitorType%22%2C%22NewLoggedOut%22%5D%7D&cvar=%7B%223%22%3A%5B%22_pks%22%2C%22SKU2%22%5D%2C%224%22%3A%5B%22_pkn%22%2C%22PRODUCT+name%22%5D%2C%225%22%3A%5B%22_pkc%22%2C%22Electronics+%26+Cameras%22%5D%7D&url=http%3A%2F%2Fexample.org%2Findex.htm&urlref=&action_name=incredible+title%21&urlref=http%3A%2F%2Fpiwik.org%2Fcontribute%',
             'referrer' => 'http://forum.piwik.org/read.php?2,76977',
             'ua'       => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1',
         );
 
+        $this->assertEquals($expected, $line);
+    }
+
+    public function test_parseLogLine_shouldHandleEmptyFields()
+    {
+        $line = LogParser::parseLogLine('- - - [29/May/2011:06:18:38 +0200] "GET /piwik.php HTTP/1.1" - - - -');
+        $expected = [
+            'ip' => '-',
+            'time' => '29/May/2011:06:18:38 +0200',
+            'url' => '/piwik.php',
+            'referrer' => '-',
+            'ua' => '-',
+        ];
         $this->assertEquals($expected, $line);
     }
 
