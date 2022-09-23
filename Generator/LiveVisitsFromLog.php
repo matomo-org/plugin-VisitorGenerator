9695ca9a82936778c4a6a5d73d12a2d321b8a926
diosmosis <diosmosis@users.noreply.github.com>
2022-09-22 17:10:25 -0700

    prefix psr\log namespace

--- Generator/LiveVisitsFromLog.php
+++ Generator/LiveVisitsFromLog.php
@@ -14,7 +14,7 @@ use Piwik\Http;
 use Piwik\Plugins\VisitorGenerator\Faker\Request;
 use Piwik\Plugins\VisitorGenerator\Iterator\TransformIterator;
 use Piwik\Plugins\VisitorGenerator\LogParser;
-use Psr\Log\LoggerInterface;
+use Matomo\Dependencies\Psr\Log\LoggerInterface;
 
 class LiveVisitsFromLog extends VisitsFromLogs
 {
